ee295724062491606f59b237da8b7df46dbc69b9
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2024-11-09 14:30:49 +0330

    feat: add constants with assets for easeier use of logo and aside

---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+export const logo = {
+  desktop: {
+    src: '/assets/images/logo-desktop.svg',
+    width: 179,
+    heigth: 50,
+  },
+  laptop: {
+    src: '/assets/images/logo-mobile.svg',
+    width: 143,
+    heigth: 40,
+  },
+  mobile: {
+    src: '/assets/images/mobile.svg',
+    width: 125,
+    heigth: 35,
+  },
+};
+
+export const asideAuthLayoutImage = '/assets/images/auth-aside-layout.jpg';
