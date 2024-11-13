59ff43ea52f229789d5b276449ff5e5cf8fc3ebb
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2024-11-13 13:28:05 +0330

    chor: add user type and role

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,24 @@
+import { Models } from 'node-appwrite';
+
 export type T_NavLink = {
   path: string;
   label: string;
   icon?: string;
   isNew?: boolean;
 };
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+export interface I_User extends Models.Document {
+  $id: string;
+  name: string;
+  email: string;
+  avatarUrl: string | null;
+  avatarId: string | null;
+  firstname: string;
+  lastname: string;
+  role: UserRole;
+  liked: string[];
+}
