c13f70169046701f12c1170fe62dc388f9de5761
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2024-11-13 14:10:23 +0330

    chor: add header changes when the user is logged in or not

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { mainNavLinks } from '@/constants/mainNavLinks';
-import { T_NavLink } from '@/types';
+import { UserRole, T_NavLink } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
 import Button from '@/components/ui/Button';
@@ -9,17 +9,31 @@ import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import NavLink from '@/components/ui/NavLink';
 import Drawer from '@/components/ui/Drawer';
+import { useAuth } from '@/context/auth/AuthProvider';
+import { AUTH_PAGE_ROUTES, MAIN_PAGE_ROUTES } from '@/constants/routes';
 
 const Header = () => {
   const pathname = usePathname();
-  const [showDrawer, setShowDrawer] = useState(false);
+  const [showDrawer, setShowDrawer] = useState<boolean>(false);
+  const {
+    state: { isLoading, user },
+  } = useAuth();
+
+  const isAdmin = user?.role === UserRole.ADMIN;
+  const dashboardPath = user
+    ? isAdmin
+      ? MAIN_PAGE_ROUTES.DASHBOARD
+      : MAIN_PAGE_ROUTES.ACCOUNT
+    : AUTH_PAGE_ROUTES.SIGN_IN;
 
   return (
-    <header className="sticky left-0 top-0 z-20 bg-dark-10/70 px-4 py-5 shadow shadow-dark-15 backdrop-blur *:font-inter">
+    <header
+      className={`sticky left-0 top-0 z-20 bg-dark-10/70 px-4 py-5 shadow shadow-dark-15 backdrop-blur *:font-inter`}
+    >
       {/* Desktop menu */}
       <div className="mx-auto hidden w-full items-center justify-between lg:container md:flex">
         {/* Logo */}
-        <Link href="/">
+        <Link href={MAIN_PAGE_ROUTES.ROOT}>
           <Image src="/assets/images/logo-laptop.svg" width={144} height={40} alt="logo" />
         </Link>
         {/* Menu links */}
@@ -37,11 +51,11 @@ const Header = () => {
         </nav>
 
         {/* Auth and contact */}
-        <div className="flex items-center gap-x-5">
-          <Link href="/sign-in">
-            <Button variant="outline">Sign In</Button>
+        <div className={`flex items-center gap-x-5 transition-all ${isLoading ? 'blur-md' : ''}`}>
+          <Link href={dashboardPath}>
+            <Button variant="outline">{user && user.$id ? 'Dashboard' : 'Sign in'}</Button>
           </Link>
-          <Link href="/contact-us">
+          <Link href={MAIN_PAGE_ROUTES.CONTACT_US}>
             <Button>Contact Us</Button>
           </Link>
         </div>
@@ -49,7 +63,7 @@ const Header = () => {
 
       {/* Mobile menu */}
       <div className="flex w-full items-center justify-between md:hidden">
-        <Link href="/">
+        <Link href={MAIN_PAGE_ROUTES.ROOT}>
           <Image src="/assets/images/logo-mobile.svg" width={125} height={35} alt="logo" />
         </Link>
         <Button onClick={() => setShowDrawer(true)} variant="outline">
@@ -59,7 +73,7 @@ const Header = () => {
         {showDrawer && (
           <Drawer open={showDrawer} onClose={() => setShowDrawer(false)}>
             <div className="flex items-center justify-between">
-              <Link href="/">
+              <Link href={MAIN_PAGE_ROUTES.ROOT}>
                 <Image src="/assets/images/logo-mobile.svg" width={125} height={35} alt="logo" />
               </Link>
               <Button onClick={() => setShowDrawer(false)} variant="outline">
@@ -84,13 +98,13 @@ const Header = () => {
                 })}
               </ul>
             </nav>
-            <div>
-              <Link href="/sign-in">
+            <div className={`transition-all ${isLoading ? 'blur-md' : ''}`}>
+              <Link href={dashboardPath}>
                 <Button variant="outline" className="mb-5 w-full">
-                  Sign In
+                  {user && user.$id ? 'Dashboard' : 'Sign in'}
                 </Button>
               </Link>
-              <Link href="/contact-us">
+              <Link href={MAIN_PAGE_ROUTES.CONTACT_US}>
                 <Button className="w-full">Contact Us</Button>
               </Link>
             </div>
